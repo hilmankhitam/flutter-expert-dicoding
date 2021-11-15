74f31c7d1d7c808cfc06b8b1b881b6e5e74fed23
unknown <hilmankhitam100@gmail.com>
2021-11-15 10:19:25 +0800

    fix

--- movies/test/json_reader.dart
+++ movies/test/json_reader.dart
@@ -3,7 +3,7 @@ import 'dart:io';
 String readJson(String name) {
   var dir = Directory.current.path;
   if (dir.endsWith('/test')) {
-    dir = dir.replaceAll('/test', '');
+    dir = dir.replaceAll('/test', 'movies');
   }
   return File('$dir/test/$name').readAsStringSync();
 }
